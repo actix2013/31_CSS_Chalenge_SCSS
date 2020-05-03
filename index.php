ee6dec489863213dddc75008a0f59af012eb8fb9
Guillaume CAVALIE <actix2013@gmail.com>
2020-05-03 20:12:20 +0200

    ALL OK , PROBLEM WITH LAST RING

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="index.css">
+
+<div class="container">
+
+        <ul class="row line-one">
+            <li class = "col-2">
+                <div></div>
+            </li>
+            <li class="col-6">
+
+                My Precious</li>
+            <li class="col-2">
+                <div></div>
+            </li>
+        </ul>
+
+    <ul class="row line-two">
+        <li class="col-6"><div></div></li>
+        <li class="col-4"> The one ring </li>
+    </ul>
+
+    <ul class="row line-tree">
+        <li class="col-1">
+            <div></div>
+        </li>
+        <li class="col-1">
+            <div></div>
+        </li>
+        <li class="col-1">
+            <div></div>
+        </li>
+        <li class="col-1">
+            <div></div>
+        </li>
+        <li class="col-1">
+            <div></div>
+        </li>
+        <li class="col-1">
+            <div></div>
+        </li>
+
+
+    </ul>
+
+
+</div>
